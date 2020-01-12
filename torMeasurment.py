22b3d5f173b263d8985472f6ff021263590b31e6
root <root@nc-ass-vip.sdv.fr>
2020-01-12 18:21:05 +0100

    Update

--- torMeasurment.py
+++ torMeasurment.py
@@ -7,6 +7,7 @@ with stem.control.Controller.from_port() as controller:
   tor_pid = controller.get_pid()
   list_of_connected = stem.util.proc.connections(pid=tor_pid)
   print("Size of list %d" %  len(list_of_connected))
-  print("Mermory usage: %s" % stem.util.proc.get_memory_usage(pid=tor_pid)[1])
+  print("Mermory usage: %s (resident_size) " % stem.util.proc.get_memory_usage(pid=tor_pid)[0])
+  print("Mermory usage: %s (virtual_size) " % stem.util.proc.get_memory_usage(pid=tor_pid)[1])
   print("CPU: %s" % stem.util.proc.stats(tor_pid,stem.util.proc.Stat.CPU_UTIME))
   print("CPU: %s" % stem.util.proc.stats(tor_pid,stem.util.proc.Stat.CPU_STIME))
